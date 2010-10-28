a0e779bc6bd30957537ff90237908d54e242e717
Nikolay Bryskin <nbryskin@yandex-team.ru>
2010-10-28 17:58:57 +0400

    * config.py moved to root * added distutils setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pyconfig',
+      version='0.0.1',
+      description='Pythonic YAmo Daemon',
+      author='Nikolay Bryskin',
+      author_email='nbyrskin@yandex-team.ru',
+      py_modules=['config']
+)
